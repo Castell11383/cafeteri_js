53b65f6c435a876953c49fd8581840863d68403d
Castell11383 <rcastellanos11383@gmail.com>
2024-07-27 18:51:36 -0600

    Navbar, Header y Footer

---
+++ includes/navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar bg-body-tertiary fixed-top navbar-expand-lg bg-info subtle">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="#" alt="Bootstrap" width="30" height="24">
+        </a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav me-auto my-2 my-lg-0" style="--bs-scroll-height: 100px;">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/proyecto_js/views/inicio.php"><i class="bi bi-house-fill me-2 "></i>Inicio</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link text-black" href="/proyecto_js/views/programador/index.php" aria-expanded="false">
+                        <i class="bi bi-people-fill"></i> Clientes
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link text-black" href="/proyecto_js/views/aplicacion/index.php" aria-expanded="false">
+                        <i class="bi bi-google-play"></i> Empleados
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link text-black" href="/proyecto_js/views/tarea/index.php" aria-expanded="false">
+                        <i class="bi bi-person-workspace"></i> Pedidos
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
